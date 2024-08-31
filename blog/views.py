aad559df10d649e69b3ba5576c6163ab5fbe230b
AS-Masoud <as.masoud.1868@gmail.com>
2024-09-01 02:00:22 +0330

    pagination,comments,sidebar for author info, recent posts

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,62 @@
-from django.views.generic import TemplateView, CreateView, DetailView
+from django.views.generic import TemplateView, CreateView, DetailView, View, FormView
 from django.views.generic.edit import UpdateView, DeleteView
 from .models import Post
-from .forms import PostForm
-from django.urls import reverse_lazy
+from .forms import PostForm, CommentForm
+from django.urls import reverse_lazy, reverse
+from django.views.generic.detail import SingleObjectMixin
 
 
 class PostNewView(CreateView):
     model = Post
     template_name = 'post_new.html'
     form_class = PostForm
-    success_url = reverse_lazy('blog_detail')
+
+    def get_success_url(self):
+        # Redirect to the detail page of the created post
+        return reverse_lazy('post_detail', kwargs={'pk': self.object.pk})
+
+    # success_url = reverse_lazy('blog_detail')
 
 
-class PostDetailView(DetailView):
+class CommentGet(DetailView):
     model = Post
     template_name = 'blog_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = CommentForm()
+        return context
+
+
+class CommentPost(SingleObjectMixin, FormView):
+    model = Post
+    form_class = CommentForm
+    template_name = 'blog_detail.html'
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().post(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.post = self.object
+        comment.author = self.request.user
+        comment.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        post = self.get_object()
+        return reverse('post_detail', kwargs={'pk': post.pk})
+
+
+class PostDetailView(View):
+    def get(self, request, *args, **kwargs):
+        view = CommentGet.as_view()
+        return view(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        view = CommentPost.as_view()
+        return view(request, *args, **kwargs)
 
 
 class PostUpdateView(UpdateView):
--- news_blog/views.py
+++ news_blog/views.py
@@ -1,12 +1,28 @@
 from django.views.generic import TemplateView
-from django.views.generic import ListView
+from django.views.generic import View
 from blog.models import Post
+from django.core.paginator import Paginator
+from django.shortcuts import render
 
 
 class HomeView(TemplateView):
     template_name = 'home.html'
 
 
-class BlogView(ListView):
+class BlogView(View):
     model = Post
     template_name = 'blogs.html'
+    paginate_by = 9
+
+    def get(self, request):
+        posts = Post.objects.all()
+        paginator = Paginator(posts, self.paginate_by)
+
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        context = {
+            'post_list': page_obj
+        }
+
+        return render(request, self.template_name, context)
